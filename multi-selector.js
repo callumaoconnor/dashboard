68010c30c6976f12eca16f9cb9ffc5e398ae77ac
callumaoconnor <93537697+callumaoconnor@users.noreply.github.com>
2022-02-18 15:42:48 -0500

    Update multi-selector.js

--- multi-selector.js
+++ multi-selector.js
@@ -1,60 +1,28 @@
-   $('.project-item').each(function(){
-    projectname = $(this).children('.project-name').text();
-    projectid = $(this).children('.project-id').text();
-    $('#tagged-projects').append('<option value="'+projectid+'">'+projectname+'</option>');
-  }); 
-  
-  $('#tagged-projects').select2({
-    placeholder: 'Select projects'
-  });
-  
-    $('.collaborator-item').each(function(){
-    collaboratoreditorname = $(this).children('.collaborator-name').text();
-    collaboratoreditorid = $(this).children('.collaborator-id').text();
-    $('#Collaborators-Editor-Project').append('<option value="'+collaboratoreditorid+'">'+collaboratoreditorname+'</option>');
+  $('.selector-item').each(function(){
+    selectorname = $(this).children('.selector-name').text();
+    selectorid = $(this).children('.selector-id').text();
+    $('#Collaborators-Editor-Project').append('<option value="'+selectorid+'">'+selectorname+'</option>');
+    $('#Collaborators-Viewer-Project').append('<option value="'+selectorid+'">'+selectorname+'</option>');
+    $('#Groups-Project').append('<option value="'+selectorid+'">'+selectorname+'</option>');
+    $('#Collaborators-Article').append('<option value="'+selectorid+'">'+selectorname+'</option>');
+    $('#Groups-Article').append('<option value="'+selectorid+'">'+selectorname+'</option>');
   }); 
   
   $('#Collaborators-Editor-Project').select2({
     placeholder: 'Add editor(s)'
   });
-
-		$('.collaborator-item').each(function(){
-    collaboratorviewername = $(this).children('.collaborator-name').text();
-    collaboratorviewerid = $(this).children('.collaborator-id').text();
-    $('#Collaborators-Viewer-Project').append('<option value="'+collaboratorviewerid+'">'+collaboratorviewername+'</option>');
-  }); 
   
   $('#Collaborators-Viewer-Project').select2({
     placeholder: 'Add viewer(s)'
   });
-
-  
-  	$('.group-item').each(function(){
-    groupname = $(this).children('.group-name').text();
-    groupid = $(this).children('.group-id').text();
-    $('#Groups-Project').append('<option value="'+groupid+'">'+groupname+'</option>');
-  }); 
   
   $('#Groups-Project').select2({
     placeholder: 'Add group(s)'
   });
   
-  	$('.collaborator-item').each(function(){
-    collaboratorviewername = $(this).children('.collaborator-name').text();
-    collaboratorviewerid = $(this).children('.collaborator-id').text();
-    $('#Collaborators-Article').append('<option value="'+collaboratorviewerid+'">'+collaboratorviewername+'</option>');
-  }); 
-  
   $('#Collaborators-Article').select2({
     placeholder: 'Add contributor(s)'
   });
-
-  
-   	$('.group-item').each(function(){
-    groupname = $(this).children('.group-name').text();
-    groupid = $(this).children('.group-id').text();
-    $('#Groups-Article').append('<option value="'+groupid+'">'+groupname+'</option>');
-  }); 
   
   $('#Groups-Article').select2({
     placeholder: 'Add group(s)'
